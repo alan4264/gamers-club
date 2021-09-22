47afa834fab893812c0a1689324e18b4de5feb95
Alan <al.zhou@mail.utoronto.ca>
2021-09-22 18:14:59 +0000

    Create routes folder and controllers folder.

--- routes/posts.js
+++ routes/posts.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const posts = require('../controllers/posts');
+
+app.get('/posts', posts.index);
+
+app.get('/posts/new', (req, res) => {
+	res.render('posts/new');
+});
+
+app.get('/posts/:id', (req, res) => {
+	res.render('posts/show');
+});
+
+app.get('/posts/:id/edit', (req, res) => {
+	res.render('posts/edit');
+});
+
+router.route('/')
+	.get()
