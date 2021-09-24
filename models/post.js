66589447bb64db69994198e6ce07ac0fd95783aa
Alan <al.zhou@mail.utoronto.ca>
2021-09-24 15:29:11 +0000

    Use mysql2 npm package for database connections. No longer using sequelize.

--- models/post.js
+++ models/post.js
@@ -1,28 +1,38 @@
-const { Sequelize, DataTypes, QueryTypes } = require('sequelize');
-const sequelize = new Sequelize('mysql', 'root', '', {
-		host: 'localhost',
-		dialect: 'mysql'
-});
+// const { Sequelize, DataTypes, QueryTypes } = require('sequelize');
+// const sequelize = new Sequelize('mysql', 'root', '', {
+// 		host: 'localhost',
+// 		dialect: 'mysql'
+// });
 
-// Maybe I don't need this part. Use mysql instead, no sequelize.
-const Post = sequelize.define('Post', {
-	id: {
-		type: DataTypes.INTEGER,
-		autoIncrement: true,
-		primaryKey: true
-	},
-	title: {
-		type: DataTypes.STRING(60),
-		allowNull: false
-	},
-	content: {
-		type: DataTypes.TEXT
-	}
-});
+// // Maybe I don't need this part. Use mysql instead, no sequelize.
+// const Post = sequelize.define('Post', {
+// 	id: {
+// 		type: DataTypes.INTEGER,
+// 		autoIncrement: true,
+// 		primaryKey: true
+// 	},
+// 	title: {
+// 		type: DataTypes.STRING(60),
+// 		allowNull: false
+// 	},
+// 	content: {
+// 		type: DataTypes.TEXT
+// 	}
+// });
 
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'gamers_club'
+});
+Post = new Object();
 Post.findById = async function findById(id) {
-	const post = await sequelize.query('SELECT * FROM posts WHERE id = ?;', {replacements: [id], type: QueryTypes.SELECT });
-	return (post.length == 1 ? post[0] : null);
+	var q = "SELECT * FROM posts WHERE id = " + id;
+	const [ post ] = await connection.promise().query(q);
+	console.log(post);
+	return post.length == 0 ? null : post[0];
 }
 
 module.exports = Post;
